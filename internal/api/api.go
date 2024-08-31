a4be33a8762fd02adbb395734908ebcbcbe2ed04
Max Castro <maxcastro@lance.com.br>
2024-08-31 16:40:56 -0300

    feat: routes

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/Shumax/askme-server/internal/store/pgstore"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 )
 
 type apiHandler struct {
@@ -22,8 +24,48 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 	}
 
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	r.Get("/subscribe/{room_id}", a.handleSubscribe)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/rooms", func(r chi.Router) {
+			r.Post("/", a.handleCreateRoom)
+			r.Get("/", a.handleGetRooms)
+
+			r.Route("/{room_id}/messages", func(r chi.Router) {
+				r.Get("/", a.handleGetRoomMessages)
+				r.Post("/", a.handleCreateRoomMessages)
+
+				r.Route("/{message_id}", func(r chi.Router) {
+					r.Get("/", a.handleGetRoomMessage)
+					r.Patch("/react", a.handleReactToMessage)
+					r.Delete("/react", a.handleRemoveReactToMessage)
+					r.Patch("/answer", a.handleMarkMessageAsAnswered)
+				})
+			})
+		})
+	})
 
 	a.r = r
-
 	return a
 }
+
+func (h apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request)             {}
+func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request)            {}
+func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request)              {}
+func (h apiHandler) handleCreateRoomMessages(w http.ResponseWriter, r *http.Request)    {}
+func (h apiHandler) handleGetRoomMessages(w http.ResponseWriter, r *http.Request)       {}
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request)        {}
+func (h apiHandler) handleReactToMessage(w http.ResponseWriter, r *http.Request)        {}
+func (h apiHandler) handleRemoveReactToMessage(w http.ResponseWriter, r *http.Request)  {}
+func (h apiHandler) handleMarkMessageAsAnswered(w http.ResponseWriter, r *http.Request) {}
